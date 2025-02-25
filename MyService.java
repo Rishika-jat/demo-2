0e7878c10ce80e5c819b75411de8d44b2676b32d
Rishika Jat <163402758+Rishika-jat@users.noreply.github.com>
2025-02-25 12:19:15 +0530

    Create MyService.java

---
+++ MyService.java
@@ -0,0 +1,11 @@
+package com.javatechie.springredditclone.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
