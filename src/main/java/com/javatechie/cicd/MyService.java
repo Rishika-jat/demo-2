40a9b945e9c613451cf785b69ff56376906d3479
Rishika Jat <163402758+Rishika-jat@users.noreply.github.com>
2025-02-25 12:29:20 +0530

    Create MyService.java

---
+++ src/main/java/com/javatechie/cicd/MyService.java
@@ -0,0 +1,11 @@
+package com.javatechie.cicd;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
