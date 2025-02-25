2125eac0e2163264bd87820f126ed7139b851e08
Rishika Jat <163402758+Rishika-jat@users.noreply.github.com>
2025-02-25 12:20:21 +0530

    Create MyServiceTest.java

---
+++ src/test/java/com/javatechie/cicd/MyServiceTest.java
@@ -0,0 +1,20 @@
+package com.javatechie.springredditclone.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class MyServiceTest {
+
+    @Autowired
+    private MyService myService;
+
+    @Test
+    void testAddNumbers() {
+        int result = myService.addNumbers(2, 3);
+        assertEquals(5, result, "2 + 3 should equal 5");
+    }
+}
